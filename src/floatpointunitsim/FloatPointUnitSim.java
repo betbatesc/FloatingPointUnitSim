20f51ca48617fa13d41de2098a81739c74ca2d96
betbatesc <betbatesc@gmail.com>
2017-10-02 05:26:42 -0500

    Add Splash Screen

--- src/floatpointunitsim/FloatPointUnitSim.java
+++ src/floatpointunitsim/FloatPointUnitSim.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package floatpointunitsim;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,31 +14,18 @@ import javafx.stage.Stage;
  */
 public class FloatPointUnitSim extends Application {
     
+    public static boolean isSplashLoaded = false;
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        primaryStage.setTitle("Floating Point Simulator");
+        primaryStage.getIcons().add(new Image("file:images/Calculator.png"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);        
         primaryStage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/floatpointunitsim/SplashFXMLController.java
@@ -0,0 +1,23 @@
+package floatpointunitsim;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author alberto
+ */
+public class SplashFXMLController implements Initializable {
+    
+    @FXML
+    private StackPane rootPane;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+    }
+    
+}
